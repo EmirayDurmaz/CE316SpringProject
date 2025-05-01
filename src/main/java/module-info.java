3534b4767f640a6c6047d0a1ccc4fdb30c6db5c6
meltemdemir <meltemd127@gmail.com>
2025-05-01 22:14:18 +0300

    User output controller class created with appropriate attributes and methods.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module org.example.integratedassignmentenviroment {
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
+    requires org.json;
 
     opens com.example.iae to javafx.fxml;
     exports com.example.iae;
