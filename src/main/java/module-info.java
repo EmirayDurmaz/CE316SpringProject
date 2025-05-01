8171d54280d61aba9e7a46402c06ca4976921011
meltemdemir <meltemd127@gmail.com>
2025-05-01 23:35:48 +0300

    Main controller class created

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module org.example.integratedassignmentenviroment {
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
     requires org.json;
+    requires com.fasterxml.jackson.databind;
 
     opens com.example.iae to javafx.fxml;
     exports com.example.iae;
