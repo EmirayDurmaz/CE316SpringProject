7d0f396e60a585b0c0e4e1b76273d68fb3f3e981
EmirayDurmaz <emiray09@hotmail.com>
2025-05-01 21:54:42 +0300

    java and c compiler methods are added

---
+++ src/main/java/com/example/iae/Compilers/CCompiler.java
@@ -0,0 +1,11 @@
+package com.example.iae.Compilers;
+
+import java.io.File;
+public class CCompiler extends Compiler {
+    public static final  String COMPILER_PATH = "gcc";
+    public static final   String ARGS = "main.c -o main.exe";
+    public static final   String RUN_COMMAND = "/main.exe";
+    public CCompiler(File workingDirectory) {
+        super(workingDirectory);
+    }
+}
---
+++ src/main/java/com/example/iae/Compilers/JavaCompiler.java
@@ -0,0 +1,12 @@
+package com.example.iae.Compilers;
+
+import java.io.File;
+public class JavaCompiler extends Compiler {
+    public static final String COMPILER_PATH = "javac";
+    public static final   String ARGS = "Test.java";
+    public static final String RUN_COMMAND = "java Test";
+    public JavaCompiler(File workingDirectory) {
+        super(workingDirectory);
+    }
+}
+
