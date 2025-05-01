860dd391d6438e07062096a283d35867a16746e9
EmirayDurmaz <emiray09@hotmail.com>
2025-05-01 21:22:30 +0300

    Homework Submisson created

---
+++ src/main/java/org/example/iae/HomeworkSub.java
@@ -0,0 +1,11 @@
+package org.example.iae;
+
+public class HomeworkSub {
+    private final String expected;
+    public HomeworkSub(String expected) {
+        this.expected = expected;
+    }
+    public String getExpected() {
+        return expected;
+    }
+}
